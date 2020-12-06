deab5a31f579f8334ad0c66d8f94a96a8bd00094
Octavian Binzari <octaw13@gmail.com>
2020-12-06 21:49:29 +0200

    first

---
+++ src/render.js
@@ -0,0 +1,88 @@
+const videoElement = document.querySelector('video'),
+	startBtn = document.getElementById('startBtn'),
+	stopBtn = document.getElementById('stopBtn'),
+	videoSelectBtn = document.getElementById('videoSelectBtn');
+
+const { desktopCapturer, remote } = require('electron'),
+	{ dialog, Menu } = remote,
+	{ writeFile } = require('fs');
+
+let mediaRecorder;
+const recordedChunks = [];
+
+videoSelectBtn.onclick = getVideoSources;
+
+startBtn.onclick = (e) => {
+	mediaRecorder.start();
+	startBtn.classList.add('is-danger');
+	startBtn.innerText = 'Recording';
+};
+
+stopBtn.onclick = (e) => {
+	mediaRecorder.stop();
+	startBtn.classList.remove('is-danger');
+	startBtn.innerText = 'Start';
+};
+
+async function getVideoSources() {
+	const inputSources = await desktopCapturer.getSources({
+		types: ['window', 'screen'],
+	});
+
+	const videoOptionsMenu = Menu.buildFromTemplate(
+		inputSources.map((source) => {
+			return {
+				label: source.name,
+				click: () => selectSource(source),
+			};
+		})
+	);
+	videoOptionsMenu.popup();
+}
+
+async function selectSource(source) {
+	videoSelectBtn.innerHTML = source.name;
+
+	const constraints = {
+		audio: false,
+		video: {
+			mandatory: {
+				chromeMediaSource: 'desktop',
+				chromeMediaSourceId: source.id,
+			},
+		},
+	};
+
+	const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+	videoElement.srcObject = stream;
+	videoElement.play();
+
+	const options = { mimeType: 'video/webm; codecs=vp9' };
+	mediaRecorder = new MediaRecorder(stream, options);
+
+	mediaRecorder.ondataavailable = handleDataAvailable;
+	mediaRecorder.onstop = handleStop;
+}
+
+function handleDataAvailable(e) {
+	console.log('video data available');
+	recordedChunks.push(e.data);
+}
+
+async function handleStop(e) {
+	const blob = new Blob(recordedChunks, {
+		type: 'video/webm; codecs=vp9',
+	});
+
+	const buffer = Buffer.from(await blob.arrayBuffer());
+
+	const { filePath } = await dialog.showSaveDialog({
+		buttonLabel: 'Save video',
+		defaultPath: `vid-${Date.now()}.mkv`,
+	});
+
+	if (filePath) {
+		writeFile(filePath, buffer, () => console.log('video saved successfully!'));
+	}
+}
